ba31cd5c70482ed1025afc595cccf55c7cfdc779
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2022-01-19 10:07:43 -0500

    My poor attempt

--- Program.cs
+++ Program.cs
@@ -1,12 +1,62 @@
-﻿using System;
+﻿using System.Collections.Generic;
+
 
 namespace KatasDivisors
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        public static int[] Divisors(int n)
+        {
+            //create function named divisors
+            // assign that function an int > 1
+            var divisors = 42;
+
+
+            private static DivisorCalculator (int divisors)
+            {
+                for (var index = 0; index < divisors; index++)
+                {
+                    if (divisors % 2 == 0)
+                    {
+                        Divisors.add(divisors / index)
+                          }
+                }
+                // order integer's divisors from smallest to largest
+                Divisors.sort();
+            }
+            // make function return an array with integer's divisors
+            var Divisors = new Array[];
+
+
+            // if the function is prime, return null 
+        }
+
     }
-  }
 }
+
+// using System.Collections.Generic;
+// public class Kata
+// {
+//   public static int[] Divisors(int n)
+//         {
+//             List<int> divisors = new List<int>();
+
+//             for (int i = 2; i < n - 1; i++)
+//             {
+//                 if (n % i == 0)
+//                 {
+//                     divisors.Add(i);
+
+//                 }
+//             }
+//             if (divisors.ToArray().Length < 1)
+//             {
+//                 return null;
+//             }
+//             else
+//             {
+//                 return divisors.ToArray();
+//             }
+
+//         }
+// }
